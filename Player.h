2ec892ee6af3372e17eb3dfe7312932649b43d64
Ryan Rebbas <ryanrebbas2001@gmail.com>
2026-02-03 16:06:56 -0500

    crated a barebone Player.h

---
+++ Player.h
@@ -0,0 +1,21 @@
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream>
+#include "Map.h"
+#include "Card.h"
+#include "Orders.h"
+
+using namespace std;
+
+class Player{
+    public:
+        Player();
+        player(const Player& p);
+        Player& operator=(const Player& p);
+        friend ostream& operator<<(ostream& out, const Player& p);
+        ~Player();
+}
+
+#endif
