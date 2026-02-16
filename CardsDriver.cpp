f4498ee4f117748a499c81c15cf2e5954ab4ee4f
cameron <cameron.shortt.7@gmail.com>
2026-02-15 19:31:14 -0500

    Add files via upload

---
+++ CardsDriver.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+#include "Cards.h"
+
+
+int main() {
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+    Deck deck(25);
+    Hand hand(5, &deck);
+
+    //std::cout << deck << "\n";
+    //std::cout << hand << "\n";
+
+    //delete hand;
+    //delete deck;
+
+    return 0;
+}
